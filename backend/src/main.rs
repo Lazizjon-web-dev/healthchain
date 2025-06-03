6d809502b9a57a0957797759f2b600a229e835ca
Lazizjon-web-dev <azizjonovlazizjon64@gmail.com>
2025-06-03 08:34:12 +0500

    feat: Add actix-web and started empty server

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
+use std::{env, io};
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    HttpServer::new(move || App::new())
+        .bind("0.0.0.0:8000")?
+        .run()
+        .await
 }
